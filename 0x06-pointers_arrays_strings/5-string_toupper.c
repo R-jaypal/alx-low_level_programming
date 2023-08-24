fa27575fd71d21481bf9cae963fbcc22f659de88
R-jaypal <robertchijosh@gmail.com>
2023-08-23 17:58:01 -0700

    Caps string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @s: pointer
+ * Return: s
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
